4090a09cb5a47de7a9a5cb7a910f5772f0d28bde
Dameon Ren <inigoren43@gmail.com>
2017-09-10 10:43:51 +0100

    updated: settings.py Initial - collectstatic

--- irblog/settings.py
+++ irblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hw0vd$_20+%@_n^*!29_f@who35qn_bo8b0#c=g04ao0pigp#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.7']
 
 
 # Application definition
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GB'
 
 USE_I18N = True
 
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
