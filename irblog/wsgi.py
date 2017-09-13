2c0e50d6eefa83c1e6e0efa4e39f87e4da78faa3
Dameon Ren <inigoren43@gmail.com>
2017-09-13 19:52:02 +0100

    added whitenoise app tp settings

--- irblog/wsgi.py
+++ irblog/wsgi.py
@@ -15,5 +15,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "irblog.settings")
 
-application = get_wsgi_application()
-application2 = DjangoWhiteNoise(get_wsgi_application())
+# application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
